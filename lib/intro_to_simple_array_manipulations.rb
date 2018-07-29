387d097440cd080a957bceed9e9a9b0dd8e55f1d
Mariel Schlaefer <mschlaefer00@gmail.com>
2018-07-29 22:26:20 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,48 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop(2)  
+end
+
+def pop_with_args(array)
+  array.pop[-2..-1]
+end
+
+def using_shift(array)
+  array.shift(2)
+end
+
+def shift_with_args(array, num)
+  array.shift(num)
+end
+
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(array, elem)
+  array.insert(4, elem)
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_delete(array, string)
+ 
+end
+
+def using_delete_at
+  
+end
+
